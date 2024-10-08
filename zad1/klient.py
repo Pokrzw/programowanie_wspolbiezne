a478d0f0e123affcebdc5d5a2dcf38c81aa4392a
pulina <pulina@pulina-PC.lan>
2024-10-08 19:34:46 +0200

    teraz dziwnie dziala

--- zad1/klient.py
+++ zad1/klient.py
@@ -1,8 +1,14 @@
+import os
 loopOn = True
 while(loopOn):
-    f = open("dane.txt", "w")
-    num = input()
-    print(num)
-    f.write(num)
-    f.close()
-    # loopOn= False
+    f = open("dane.txt", "r+")
+
+    f.seek(0, os.SEEK_END)
+    if f.tell()==0:
+        num = input()
+        f.write(str(num))
+        f.close()
+    else:
+        f.close()
+        pass
+    
